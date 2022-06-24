c0467e525c27a84abeda0f0ff8123ec146012046
Dereje <getutadessedereje@icloud.com>
2022-06-24 13:27:43 +0200

    Update: change font-size for all h1 ect..

--- functions.php
+++ functions.php
@@ -7,6 +7,7 @@ add_action( 'wp_enqueue_scripts', 'ona_wp_enqueue_scripts' );
 function ona_wp_enqueue_scripts() {
     wp_enqueue_style( 'ona-wp-style', get_stylesheet_uri() );
     // charge another css file
-    // wp_enqueue_style( 'ona-wp-style-2', get_template_directory_uri() . './assets/css/style.min.css');
+    // add font awesome icons to the theme (https://fontawesome.com/?utm_source=v4_homepage&utm_medium=display&utm_campaign=fa5_released&utm_content=banner)
+    wp_enqueue_style( 'ona-wp-font-awesome', 'https://use.fontawesome.com/releases/v5.0.13/css/all.css' );
     wp_enqueue_script( 'ona-wp-script', get_template_directory_uri() . '/js/ona-wp.js');
 }
