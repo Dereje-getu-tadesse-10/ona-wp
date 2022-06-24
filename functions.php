86a44a3a821db97e2b2793960b6cc2d98d7c57a1
Dereje <getutadessedereje@icloud.com>
2022-06-24 12:55:30 +0200

    Update: add section presentation

--- functions.php
+++ functions.php
@@ -6,5 +6,7 @@ add_action( 'wp_enqueue_scripts', 'ona_wp_enqueue_scripts' );
 
 function ona_wp_enqueue_scripts() {
     wp_enqueue_style( 'ona-wp-style', get_stylesheet_uri() );
+    // charge another css file
+    // wp_enqueue_style( 'ona-wp-style-2', get_template_directory_uri() . './assets/css/style.min.css');
     wp_enqueue_script( 'ona-wp-script', get_template_directory_uri() . '/js/ona-wp.js');
 }
