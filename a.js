8a4e3f63aa51b542461e4cc6b9ee64cb6fc6ca84
Dereje <getutadessedereje@icloud.com>
2022-06-28 14:44:00 +0200

    Update: reponsive big screen

--- a.js
+++ a.js
@@ -1,4 +1,35 @@
-// after window load event alert hello world
-window.addEventListener('load', () => {
-    alert('Hello World');
-});
+
+
+// window.addEventListener("load", () => {
+//     setTimeout(t, 2000);
+
+//     i(g)
+// })
+
+const t = () => {
+    const i = Array.from(document.querySelectorAll('.stats-container'));
+    i.forEach(el => { 
+        let d = el.children[1].children[1].previousElementSibling.firstChild.data;
+        let val =  el.children[1].children[1].previousElementSibling.firstChild.data;
+        // console.log(val);
+
+        // grind number step by step to max value without gsap
+      
+        for (let i = 0; i < val; i++) {
+            setTimeout(() => {
+                el.children[1].children[1].previousElementSibling.firstChild.data = i +1;
+            }, i * 10);
+        }
+
+    });
+}
+t();
+const i = () => {
+    console.log(y);
+    const e = document.querySelector('.wp-container-1');
+    const f = document.querySelector('.wp-container-4');
+    const s = document.querySelector('.wp-container-6');
+    const g = document.querySelector('.wp-container-9');
+    const a = document.querySelector('.wp-container-12');
+
+}
--- functions.php
+++ functions.php
@@ -4,6 +4,9 @@ add_action( 'wp_enqueue_scripts', 'ona_wp_enqueue_style' );
 
 function ona_wp_enqueue_style() {
     wp_enqueue_style( 'ona-wp-style', get_stylesheet_uri() );
-    wp_enqueue_style( 'ona-wp-font-awesome', 'https://use.fontawesome.com/releases/v5.0.13/css/all.css' );
+    // charge font awesome entete
+    wp_enqueue_style( 'font-awesome', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css' );
+    wp_enqueue_script( 'gsap', 'https://cdnjs.cloudflare.com/ajax/libs/gsap/3.10.4/gsap.min.js', array(), false, true );
+    wp_enqueue_script( 'script', get_theme_file_uri("a.js"), array(), null, true );
 }
 
